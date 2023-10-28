2d72c0128ab09e965b1697e59f40d2cbcefb6752
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-28 20:52:59 +1100

    Finished with Project/s component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SectionHeading from "./SectionHeading";
 import { projectsData } from "@/app/lib/data";
-import Image from "next/image";
+import Project from "./Project";
 
 export default function Projects() {
 	return (
@@ -14,46 +14,6 @@ export default function Projects() {
 					</React.Fragment>
 				))}
 			</div>
-		</section>
-	);
-}
-
-// Good practice to name custom types with componentname then props
-// custom type ProjectProps can be a type of any number of these objects from projectsData
-type ProjectProps = (typeof projectsData)[number];
-// The above is more specific than:
-// type ProjectProps = {
-//     title: string;
-//     description: string;
-//     tags: string;
-//     imageUrl: string;
-// }
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-	return (
-		<section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative">
-			<div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]">
-				<h3 className="text-2xl font-semibold">{title}</h3>
-				<p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-				<ul className="flex flex-wrap mt-4 gap-2">
-					{tags.map((tag, index) => (
-						<li
-							className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-							key={index}>
-							{tag}
-						</li>
-					))}
-				</ul>
-			</div>
-
-			<Image
-				src={imageUrl}
-				alt="Project I worked on."
-				width={650}
-				height={650}
-				quality={95}
-				className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
-			/>
 		</section>
 	);
 }
--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -5,5 +5,5 @@ type ScetionHeadingProps = {
 };
 
 export default function SectionHeading({ children }: ScetionHeadingProps) {
-	return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+	return <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>;
 }
