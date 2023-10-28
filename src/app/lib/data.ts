fda95f7fc2c1709873635967836491ecbfa83525
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-28 19:00:11 +1100

    Initial Projects component & Project cards/component added

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -15,6 +15,9 @@ const wordanalyticsImg = "/wordanalytics.png";
 // the const assertion will treat each individual item in 'links' as an object with specific properties and values.
 // Additionally, these objects, as well as the 'links' array itself, become read-only.
 
+// It's good to use this when data is persitently hardcoded, but not a good idea to
+// as const when fetching data from an API or backend for exampke
+
 
 export const links = [
 	{
@@ -43,6 +46,27 @@ export const links = [
 	},
 ] as const;
 
+export const projectsData = [
+	{
+		title: "Project 1",
+		description: "Some description about this project, a short summary. More description to describe the project, it is a full-stack project.",
+		tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+		imageUrl: corpcommentImg,
+	},
+	{
+		title: "Project 2",
+		description: "Some description about this project, a short summary. More description to describe the project, it is a full-stack project.",
+		tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+		imageUrl: rmtdevImg,
+	},
+	{
+		title: "Project 3",
+		description: "Some description about this project, a short summary. More description to describe the project, it is a full-stack project.",
+		tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+		imageUrl: wordanalyticsImg,
+	},
+] as const;
+
 export const experiencesData = [
 	{
 		title: "Diploma of Information Technology",
@@ -66,27 +90,6 @@ export const experiencesData = [
 			"Worked as a freelance camera & lighting technician in diverse projects for platforms such as Netflix and Stan and brands such as Adobe, Virgin Airlines, and Tourism Australia.",
 		icon: React.createElement(FaReact),
 		date: 2023,
-	},
-] as const;
-
-export const projectsData = [
-	{
-		title: "Project 1",
-		description: "Some description about this project, a short summary.",
-		tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-		imageUrl: corpcommentImg,
-	},
-	{
-		title: "Project 2",
-		description: "Some description about this project, a short summary.",
-		tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-		imageUrl: rmtdevImg,
-	},
-	{
-		title: "Project 3",
-		description: "Some description about this project, a short summary.",
-		tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-		imageUrl: wordanalyticsImg,
 	},
 ] as const;
 
