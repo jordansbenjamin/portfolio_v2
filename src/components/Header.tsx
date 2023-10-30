69f42d5801867a6ec2240b18f8532f49bd6e0194
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-30 18:00:30 +1100

    Added dark mode styling for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
 		<header className="z-[999] relative">
 			{/* Nav container */}
 			<motion.div
-				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
 				initial={{ y: -500, x: "-50%", opacity: 0 }}
 				animate={{ y: 0, x: "-50%", opacity: 1 }}></motion.div>
 
@@ -33,8 +33,9 @@ export default function Header() {
 							initial={{ y: -500, opacity: 0 }}
 							animate={{ y: 0, opacity: 1 }}>
 							<Link
-								className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition", {
-									"text-gray-950": activeSection === link.name,
+								// clsx is a custom tailwind function
+								className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300", {
+									"text-gray-950 dark:text-gray-200": activeSection === link.name,
 								})}
 								href={link.hash}
 								// Changing active link when link is clicked
@@ -46,7 +47,7 @@ export default function Header() {
 								{/* Conditionally render the active link pill BG */}
 								{link.name === activeSection && (
 									<motion.span
-										className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+										className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
 										layoutId="activeSection"
 										transition={{ type: "spring", stiffness: 380, damping: 30 }}></motion.span>
 								)}
