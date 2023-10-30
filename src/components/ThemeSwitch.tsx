0eefdc3099ffbd2bea1978ad9a7b2bae31274bdf
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-30 17:43:32 +1100

    Implemented local storage for tracking theme mode state

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 // union type
@@ -13,10 +13,37 @@ export default function ThemeSwitch() {
 	const toggleTheme = () => {
 		if (theme === "light") {
 			setTheme("dark");
+			// persist state to local storage
+			// This is done on this hadnler because its based user actions
+			// in this case, toggling the theme on and off
+			window.localStorage.setItem("theme", "dark");
 		} else {
 			setTheme("light");
+			window.localStorage.setItem("theme", "light");
 		}
 	};
+
+	// To synchronise with an external system, in this case local storage
+	// useEffect is a great use case for that
+	// Everytime the user toggles the theme, it will set the local storage with
+	// that respective theme, and we use localstorage because we want that info
+	// to persist. And we use useEffect here, because we need to get the persisted
+	// theme data from local storage everytime the user re-load or re-visits the page
+
+	// componentOnMount because its only when the component loads,
+	// so when the user perhaps revisit the page later
+	useEffect(() => {
+		const localTheme = window.localStorage.getItem("theme") as Theme | null;
+
+		// if local theme is not null (exists)
+		if (localTheme) {
+			// set the theme with the currently saved local theme
+			setTheme(localTheme);
+			// if nothing is on local storage we want to check the users computer settings
+		} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+			setTheme("dark");
+		}
+	}, []);
 
 	return (
 		<button
