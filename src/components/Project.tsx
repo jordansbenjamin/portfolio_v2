ff5f08a87a01120efd2f632814fc80f30a740b60
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-30 19:07:12 +1100

    Added some changes for responsive breakpoints

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -37,9 +37,9 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
 	return (
 		<motion.div className="group mb-3 sm:mb-8 last:mb-0" ref={ref} style={{ scale: scaleProgress, opacity: opacityProgress }}>
 			{/* Project container */}
-			<section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+			<section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
 				{/* Left content container */}
-				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
 					{/* Project title */}
 					<h3 className="text-2xl font-semibold">{title}</h3>
 					{/* Projetc desc */}
@@ -62,7 +62,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
 					width={650}
 					height={650}
 					quality={95}
-					className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:-right-[intial] group-even:-left-40 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+					className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:-right-[intial] group-even:-left-40 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
 				/>
 			</section>
 		</motion.div>
