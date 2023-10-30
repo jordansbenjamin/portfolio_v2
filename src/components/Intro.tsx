ca2e921b65d1f1ed6132284695e90abeaf882b67
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-30 18:04:30 +1100

    Updated icon styling for intro section to dark mode

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -83,21 +83,21 @@ export default function Intro() {
 				<a
 					href="/CV.pdf"
 					download
-					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack">
+					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60">
 					Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
 				</a>
 				{/* LinkedIn link */}
 				<a
 					href="https://www.linkedin.com/in/jordansb/"
 					target="_blank"
-					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack">
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60">
 					<BsLinkedin />
 				</a>
 				{/* GitHub link */}
 				<a
 					href="https://github.com/jordansbenjamin"
 					target="_blank"
-					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack">
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition borderBlack dark:bg-white/10 dark:text-white/60">
 					<FaGithubSquare />
 				</a>
 			</motion.div>
